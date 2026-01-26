72dc5314137a86c4099296a2e9f73f9daae6f575
Pedro Candelaresi <pcandelaresi.nqn@gmail.com>
2026-01-26 13:44:19 -0300

    ventas modal detalle

--- src/modules/users-auth/entities/user.entity.ts
+++ src/modules/users-auth/entities/user.entity.ts
@@ -1,22 +1,28 @@
 import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
 import { UserProvider } from '../../users-auth/entities/user-provider.entity';
 import { UserRole } from '../../users-auth/entities/user-role.entity';
 import { RefreshToken } from '../../users-auth/entities/refresh-token.entity';
 
+@ObjectType()
 @Entity({ name: 'mudras_usuarios_auth' })
 export class UserAuth {
+  @Field(() => ID)
   @PrimaryColumn({ type: 'char', length: 36 })
   id!: string;
 
+  @Field({ nullable: true })
   @Column({ type: 'varchar', length: 191, nullable: true, unique: true })
   username!: string | null;
 
+  @Field({ nullable: true })
   @Column({ type: 'varchar', length: 191, nullable: true, unique: true })
   email!: string | null;
 
   @Column({ name: 'password_hash', type: 'varchar', length: 255, nullable: true })
   passwordHash!: string | null;
 
+  @Field()
   @Column({ name: 'display_name', type: 'varchar', length: 191 })
   displayName!: string;
 
